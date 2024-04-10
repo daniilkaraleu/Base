fa936d7acb4765e31e5c0075302f6abfabbdea09
daniilkaraleu <161074346+daniilkaraleu@users.noreply.github.com>
2024-04-10 14:27:49 +0300

    Completed observation and Novice tasks

---
+++ Base/Module 3/Observation.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int const LENGTH = 11;
+//
+//int findMax(int array[]);
+//
+//int main(){
+//    double maxA = 0;
+//    double a = 0;
+//    double observerPosition = 0;
+//    double location[LENGTH] = {7,4,5,6,8,9,0,3,9,23,75};
+//
+//    observerPosition = location[0];
+//    maxA = (location[1] - observerPosition);
+//    cout << location[1] << " ";
+//
+//    for (int i = 2; i < LENGTH; ++i) {
+//        a = (location[i] - observerPosition) /  i;
+//        if (maxA <= a){
+//            maxA = a;
+//            cout << location[i] << " ";
+//        }
+//    }
+//    return 0;
+//}
+//
