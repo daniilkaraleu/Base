b3b79f4ee86358f52052862b402ec97ff86fa693
daniilkaraleu <161074346+daniilkaraleu@users.noreply.github.com>
2024-04-20 21:21:53 +0300

    Done refactoring Daniil

--- Base/Module 4/CheckPlagiarism.cpp
+++ Base/Module 4/CheckPlagiarism.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+int const N = 4096;
+int const AMOUNT_OF_WORDS = 3;
+int const nOfPrepositions = 11;
+int const nOfArticles = 3;
 
 string text = "Pheochromocytoma and paraganglioma are rare tumors arising from adrenal medullary "
               "chromaffin cells or extraadrenal sympathetic neurons, respectively. In most other respects, "
@@ -10,90 +15,88 @@ string text = "Pheochromocytoma and paraganglioma are rare tumors arising from a
               "Carcinoid is also a neuroendocrine tumor and shares several cytochemical features with Pheo. "
               "For the treatment of Pheo and carcinoid, surgery, chemotherapy, and radiotherapy are used. "
               "Once metastasis has occurred, treatment has met with limited success, and these treatments are themselves the cause of untoward side-effects. "
-              "A promising treatment, originally developed by investigators at the University of Michigan, is the use of metaiodobenzylguanidine (MIBG) labeled with iodine131."
+              "A promising treatment, originally developed by investigators at the University of Michigan, is the use of metaiodobenzylguanidine (MIBG) labeled with iodine131. "
               "1–8 MIBG is accumulated by neuroendocrine cancer cells via the norepinephrine transporter. "
-              "Beta-particles emitted by 131I deliver energy that is cytotoxic; it is the combination of 131I cytotoxicity and MIBG target specificity that results in a selective, potentially lethal dose to tumor cells. "
+              "Beta-particles emitted by 131I deliver energy that is cytotoxic; "
+              "it is the combination of 131I cytotoxicity and MIBG target specificity that results in a selective, potentially lethal dose to tumor cells. "
               "Iodine-131 also emits gamma photons that can be detected by using a gamma camera for imaging the distribution of the radiopharmaceutical ";
-string separators = " ,!?:.\n\t-(){}[];";
-int const N = 4096;
 
+string separators = " ,!?:.\n\t-(){}[];";
 int carriage = 0;
 bool notTheEnd = true;
-int const AMOUNT_OF_WORDS = 2;
 string shinglesText[N] = {};
 string shinglesFragment[N] = {};
-int amountOfShinglesFragment = 0;
+string articles[nOfArticles] = {"AN", "THE", "A"};
+string prepositions[nOfPrepositions] = {"AT", "BY", "AS", "IN",
+                                        "FROM", "INTO", "TO",
+                                        "FOR", "FROM", "ON", "OF"};
 
-
+// Plagiarism check algorithm
 int checkPlagiarism(string text, string fragment);
-int makeShinglesArray(string text, string shingles[], int index);
+void makeShinglesArray(string text, string shingles[], int index);
 bool compareShingles(string shingleText, string shingleFragment);
 int getStringLength(string str);
 double getTotalCoincidence(string fragment, string text);
-int countPlagiarism(int amountOfShinglesText, string shingle);
-void setShinglesNull(string shingle[N]);
+int findCoincidence(int amountOfShinglesText, string shingle);
+void resetShingles(string shingle[N]);
 int roundedToInt(double num);
 int skipOneWord(string str, int index);
 int getAmountOfShingles(string shingle[]);
-string makeShingleFragment(string fragment);
-void changeFragment(string& fragment, bool deletedShingle);
-void removeShingle(string& fragment);
-void removeFirstWord(string& text);
-double count(string& fragment, string text);
-
-
-
-
-void cleanStr(string& str);
-void replaceSeparatorsWithWhitespaces(string& str);
+string makeShingleOfFragment(string fragment);
+void changeFragment(string &fragment, bool deletedShingle);
+void removeShingle(string &fragment);
+void removeFirstWord(string &text);
+double calculateResultPlagiarism(string &fragment, string text);
+// Text preprocessing algorithm
+void cleanStr(string &str);
+void replaceSeparatorsWithWhitespaces(string &str);
 bool isSeparator(char sym);
-void removeExcessSpaces(string& str);
-void removeArticles(string& str);
-void removePrepositions(string& str);
-void capitalize(string& str);
-void deleteSubstring(string& str, string& subStr);
-void deleteNonLatinSymbols(string& str);
-void deleteDuplicates(string& str);
-void copyStr(string& str1, string str2);
+void removeExcessSpaces(string &str);
+void removeArticles(string &str);
+void removePrepositions(string &str);
+void capitalize(string &str);
+void deleteSubstring(string &str, string &subStr);
+void deleteNonLatinSymbols(string &str);
+void deleteDuplicates(string &str);
+void copyStr(string &str1, string str2);
 bool isSame(string str1, string str2);
-void concatStr(string& str1, string str2);
+void concatStr(string &str1, string str2);
 
 int main() {
     string fragment = "Carcinoid is also tumor and shares several cytochemical features with Pheo.";
     int choice = 0;
+
     cout << text << endl;
     cout << endl;
 
     while (true) {
         cout << "1) Verify new fragment \n2) Exit" << endl;
         cin >> choice;
-        if (choice == 1) {
+
+        if (choice != 1) {
+            break;
+        } else {
             cout << "Input your text:" << endl;
             getline(cin, fragment);
             getline(cin, fragment);
             cout << "Your plagiarism percent: " << checkPlagiarism(text, fragment) << "%" << endl;
-            setShinglesNull(shinglesText);
-            setShinglesNull(shinglesFragment);
+            resetShingles(shinglesText);
+            resetShingles(shinglesFragment);
             carriage = 0;
             notTheEnd = true;
-        } else {
-            break;
         }
     }
-
-
     return 0;
 }
 
-int checkPlagiarism(string text, string fragment){
+int checkPlagiarism(string text, string fragment) {
     cleanStr(text);
     cleanStr(fragment);
 
-    return roundedToInt(count(fragment, text));
+    return roundedToInt(calculateResultPlagiarism(fragment, text));
 }
 
-void cleanStr(string& str) {
-
+void cleanStr(string &str) {
     replaceSeparatorsWithWhitespaces(str);
     removeExcessSpaces(str);
     capitalize(str);
@@ -107,8 +110,10 @@ int getStringLength(string str) {
     int counter = 0;
 
     while (true) {
-        if (str[counter] == '\0')
+        if (str[counter] == '\0'){
             break;
+        }
+
         counter++;
     }
     return counter + 1;
@@ -116,23 +121,26 @@ int getStringLength(string str) {
 
 bool isSeparator(char sym) {
     for (int i = 0; separators[i] != '\0'; i++) {
-        if (sym == separators[i])
+        if (sym == separators[i]){
             return true;
+        }
     }
+
     return false;
 }
 
-void replaceSeparatorsWithWhitespaces(string& str) {
+void replaceSeparatorsWithWhitespaces(string &str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (isSeparator(str[i]))
             str[i] = ' ';
     }
 }
 
-void removeExcessSpaces(string& str) {
+void removeExcessSpaces(string &str) {
     string stack = "";
-
+    int stackLength = 0;
     int spaceCounter = 0;
+
     while (str[spaceCounter] == ' ') {
         spaceCounter++;
     }
@@ -143,14 +151,16 @@ void removeExcessSpaces(string& str) {
         else
             stack += str[i];
     }
-    int stackLength = getStringLength(stack);
-    if (stack[stackLength - 1] == ' ')
+
+    stackLength = getStringLength(stack);
+    if (stack[stackLength - 1] == ' '){
         stack[stackLength - 1] = '\0';
+    }
 
     copyStr(str, stack);
 }
 
-void capitalize(string& str) {
+void capitalize(string &str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' and str[i] <= 'z') {
             str[i] = str[i] - 32;
@@ -158,88 +168,86 @@ void capitalize(string& str) {
     }
 }
 
-void deleteSubstring(string& str, string& subStr) {
+void deleteSubstring(string &str, string &subStr) {
     string tempWord = "";
     string stack = "";
+
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != ' ') {
             tempWord += str[i];
-        }
-        else if (!isSame(tempWord, subStr)) {
+        } else if (!isSame(tempWord, subStr)) {
             stack += tempWord + " ";
             tempWord = "";
-        }
-        else {
+        } else {
             tempWord = "";
         }
     }
+
     if (!isSame(tempWord, subStr)) {
         concatStr(stack, tempWord);
         tempWord = "";
-        }
+    }
 
     copyStr(str, stack);
 }
 
-void removeArticles(string& str) {
-    int const nOfArticles = 3;
-
-    string articles[nOfArticles] = {"AN", "THE", "A"};
-    for (int i = 0; i < nOfArticles; i++){
+void removeArticles(string &str) {
+    for (int i = 0; i < nOfArticles; i++) {
         deleteSubstring(str, articles[i]);
     }
 }
 
-void removePrepositions(string& str) {
-    int const nOfPrepositins = 11;
-    string prepositions[nOfPrepositins] = {"AT", "BY", "AS", "IN", "FROM", "INTO", "AT", "FOR", "FROM", "ON", "OF"};
-    for (int i = 0; i < nOfPrepositins; i++){
+void removePrepositions(string &str) {
+    for (int i = 0; i < nOfPrepositions; i++) {
         deleteSubstring(str, prepositions[i]);
     }
 }
 
-void deleteDuplicates(string& str) {
+void deleteDuplicates(string &str) {
     string tempWord = "";
     string preTempWord = "";
     string stack = "";
+
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != ' ') {
             tempWord += str[i];
-        }
-        else if (str[i] == ' ') {
+        } else if (str[i] == ' ') {
             if (tempWord != preTempWord) {
                 stack += tempWord + " ";
             }
             preTempWord = tempWord;
             tempWord = "";
         }
+
         if (str[i + 1] == '\0') {
             stack += tempWord;
         }
     }
+
     copyStr(str, stack);
 }
 
-void deleteNonLatinSymbols(string& str) {
+void deleteNonLatinSymbols(string &str) {
     string tempWord = "";
     string stack = "";
 
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != ' ') {
             tempWord += str[i];
-            if ((str[i] < 'A' or str[i] > 'Z')and str[i] != '\'') {
+            if ((str[i] < 'A' or str[i] > 'Z') and str[i] != '\'') {
                 tempWord = "";
                 continue;
             }
-        }
-        else if (str[i] == ' ') {
+        } else if (str[i] == ' ') {
             stack += tempWord + " ";
             tempWord = "";
         }
+
         if (str[i + 1] == '\0') {
             concatStr(stack, tempWord);
         }
     }
+
     copyStr(str, stack);
 }
 
@@ -260,46 +268,50 @@ bool isSame(string str1, string str2) {
     return isSameStrings;
 }
 
-int makeShinglesArray(string text, string shingles[], int index) {
+void makeShinglesArray(string text, string shingles[], int index) {
     int spaces = 0;
     int iShingle = 0;
     int i = 0;
 
-    for (i = index; text[i] != '\0';) {
-        if (text[i] == '\0')
-            break;
-        for (spaces = 0; spaces < AMOUNT_OF_WORDS;) {
-            if (text[i] == ' '){
+    i = index;
+    while (text[i] != '\0') {
+        spaces = 0;
+
+        while (spaces < AMOUNT_OF_WORDS) {
+            if (text[i] == ' ') {
                 while (text[i + 1] == ' ')
                     i++;
                 spaces++;
-            }
-            else if (text[i] == '\0') {
+            } else if (text[i] == '\0') {
                 break;
-            }
-            else
+            } else {
                 shingles[iShingle] += text[i];
+            }
             i++;
         }
+
         iShingle++;
     }
-    if (text[i-1] == ' ')
-        spaces--;
-    if (spaces < AMOUNT_OF_WORDS - 1){
-        shingles[iShingle - 1] = "";
-        return iShingle - 1;
-    }
 
-    return iShingle;
+    if (text[i - 1] == ' ') {
+        spaces--;
+    }
+    if (spaces < AMOUNT_OF_WORDS - 1) {
+        shingles[iShingle - 1] = "";
+    }
 }
 
 bool compareShingles(string shingleText, string shingleFragment) {
-    if (getStringLength(shingleText) != getStringLength(shingleFragment))
+    if (getStringLength(shingleText) != getStringLength(shingleFragment)){
         return false;
-    for (int i = 0; shingleFragment[i] != 0; ++i) {
-        if (shingleFragment[i] != shingleText[i])
-            return false;
     }
+
+    for (int i = 0; shingleFragment[i] != 0; ++i) {
+        if (shingleFragment[i] != shingleText[i]){
+            return false;
+        }
+    }
+
     return true;
 }
 
@@ -314,72 +326,81 @@ double getTotalCoincidence(string fragment, string text) {
     bool deletedShingle = false;
 
     makeShinglesArray(fragment, shinglesFragment, indexFragment);
-    amountOfShinglesFragment = getAmountOfShingles(shinglesFragment);
+
     while (notTheEnd) {
-        shingle = makeShingleFragment(fragment);
+        shingle = makeShingleOfFragment(fragment);
+
         for (int i = 0; i < AMOUNT_OF_WORDS; ++i) {
             makeShinglesArray(text, shinglesText, indexText);
             amountOfShinglesText = getAmountOfShingles(shinglesText);
-            plagiarism = countPlagiarism(amountOfShinglesText, shingle);
+            plagiarism = findCoincidence(amountOfShinglesText, shingle);
             total += plagiarism;
-            setShinglesNull(shinglesText);
+            resetShingles(shinglesText);
             indexText = skipOneWord(text, indexText);
 
-            if (plagiarism){
+            if (plagiarism) {
                 deletedShingle = true;
                 break;
             }
         }
+
         changeFragment(fragment, deletedShingle);
-        shingle="";
+        shingle = "";
         deletedShingle = false;
     }
+
     return total;
 }
 
-void copyStr(string& str1, string str2) {
+void copyStr(string &str1, string str2) {
     char copy[N] = {};
     int i = 0;
-    for(i = 0; str2[i] != '\0'; ++i) {
+
+    for (i = 0; str2[i] != '\0'; ++i) {
         copy[i] = str2[i];
     }
+
     copy[i] = '\0';
     str1 = copy;
 }
 
-void concatStr(string& str1, string str2) {
+void concatStr(string &str1, string str2) {
     char copy[N] = {};
     int i = 0;
     int j = 0;
+
     for (i = 0; str1[i] != '\0'; i++) {
         copy[i] = str1[i];
     }
+
     for (j = 0; str2[j] != '\0'; j++) {
         copy[i] = str2[j];
         i++;
     }
+
     copy[i] = '\0';
     str1 = copy;
 }
 
-int skipOneWord(string str, int index){
+int skipOneWord(string str, int index) {
     int i = index;
 
-    while (str[i] != ' '){
+    while (str[i] != ' ') {
         i++;
     }
 
-    while (str[i] == ' ')
+    while (str[i] == ' '){
         i++;
+    }
 
     return i;
 }
 
-int countPlagiarism(int amountOfShinglesText, string shingle) {
+int findCoincidence(int amountOfShinglesText, string shingle) {
     int plagiarism = 0;
 
     for (int j = 0; j < amountOfShinglesText; ++j) {
-        if (compareShingles(shinglesText[j], shingle)){
+        if (compareShingles(shinglesText[j], shingle)) {
             plagiarism++;
             break;
         }
@@ -388,13 +409,13 @@ int countPlagiarism(int amountOfShinglesText, string shingle) {
     return plagiarism;
 }
 
-void setShinglesNull(string shingle[N]){
+void resetShingles(string shingle[4096]) {
     for (int i = 0; i < N - 1; ++i) {
         shingle[i] = "";
     }
 }
 
-int roundedToInt(double num){
+int roundedToInt(double num) {
     int result = 0;
 
     result = num + 0.5;
@@ -402,68 +423,89 @@ int roundedToInt(double num){
     return result;
 }
 
-int getAmountOfShingles(string shingle[]){
+int getAmountOfShingles(string shingle[]) {
     int counter = 0;
 
-    while (shingle[counter] != "")
+    while (shingle[counter] != "") {
         counter++;
+    }
+
     return counter;
 }
 
-string makeShingleFragment(string fragment){
+string makeShingleOfFragment(string fragment) {
     string shingle;
     int spaces = 0;
 
-    while (fragment[carriage] == ' ')
+    while (fragment[carriage] == ' ') {
         carriage++;
-    for (int spaces = 0; spaces < AMOUNT_OF_WORDS;) {
-        if (fragment[carriage] == ' '){
-            while (fragment[carriage + 1] == ' ')
+    }
+
+    spaces = 0;
+    while (spaces < AMOUNT_OF_WORDS) {
+        if (fragment[carriage] == ' ') {
+            while (fragment[carriage + 1] == ' ') {
                 carriage++;
+            }
+
             spaces++;
-        }else if (fragment[carriage] == '\0'){
+        } else if (fragment[carriage] == '\0') {
             notTheEnd = false;
             break;
-        }else
+        } else {
             shingle += fragment[carriage];
+        }
+
         carriage++;
     }
 
     return shingle;
 }
-void changeFragment(string& fragment, bool deletedShingle){
-    if (!deletedShingle)
+
+void changeFragment(string &fragment, bool deletedShingle) {
+    if (!deletedShingle) {
         removeFirstWord(fragment);
-    else
+    } else {
         removeShingle(fragment);
+    }
 }
 
-void removeFirstWord(string& text){
+void removeFirstWord(string &text) {
     int index = 0;
-    while (text[index] == ' ')
+
+    while (text[index] == ' ') {
         index++;
-    while (text[index] != ' '){
+    }
+
+    while (text[index] != ' ') {
         text[index] = ' ';
         index++;
     }
+
     carriage = index;
 }
-void removeShingle(string& fragment){
+
+void removeShingle(string &fragment) {
     for (int i = 0; i < carriage - 1; ++i) {
         fragment[i] = ' ';
     }
+
     carriage = 0;
 }
-double count(string& fragment, string text){
+
+double calculateResultPlagiarism(string &fragment, string text) {
     double result = 0.0;
     double plagiarism = 0.0;
+    int amountOfShingles = 0;
 
     plagiarism = getTotalCoincidence(fragment, text);
+    amountOfShingles = getAmountOfShingles(shinglesFragment);
 
-    if (amountOfShinglesFragment == 0)
+    if (amountOfShingles == 0) {
         result = 0;
-    else
-        result = plagiarism / amountOfShinglesFragment * 100.0;
+    } else {
+        result = plagiarism / amountOfShingles * 100.0;
+    }
 
     return result;
 }
